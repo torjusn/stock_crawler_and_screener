f34612b62b41d05c87b681aebd62854c60f791a7
torjusn <tornil1996@hotmail.com>
2022-10-01 13:17:26 +0200

    ss

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+# -----------------------------------------------------------
+# company stock crawler and screener
+#
+# Torjus Nilsen, Drammen, Norway
+# email tornil1996@hotmail.com
+# -----------------------------------------------------------
+
 import yfinance as yf
 
